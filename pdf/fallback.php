85fc13ba3362300342077783e2a798b0ea0f26c3
julian <julian@c9bd92c4-aa05-45fb-8fbd-673d4b3f6551>
2012-06-20 05:58:18 +0000

    Fix usage of fallback class
    Signed-off-by: Julian Calaby <julian.calaby@gmail.com>

--- pdf/fallback.php
+++ pdf/fallback.php
@@ -3,8 +3,8 @@
 include_once("pdf/lib/pdfBase.php");
 
 class fallback extends pdfBase {
-	public function __construct() {
-		parent::__construct();
+	public function __construct($classname, $docclass) {
+		parent::__construct($docclass);
 
 		$this->setMessage("PDF class not defined");
 	}
