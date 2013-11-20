6137f1840791f30b30cdc8a2911d0816b26ee3f4
julian <julian@c9bd92c4-aa05-45fb-8fbd-673d4b3f6551>
2013-11-20 03:52:49 +0000

    Add support for custom headers and footers in pdf pages
    Signed-off-by: Julian Calaby <julian.calaby@gmail.com>

--- lib/libPDFInterface.php
+++ lib/libPDFInterface.php
@@ -5,8 +5,11 @@
  * This is for the RTF / PDF switching stuff
  */
 
+include_once("lib/libPDFListener.php");
+
 interface libPDFInterface {
 	public function getMimeType();
 	public function getExtension();
 	public function getContent();
+	public function addListener(libPDFListener $class);
 }
---
+++ lib/libPDFListener.php
@@ -0,0 +1,14 @@
+<?php
+
+/* PDF Listener
+ *
+ * Events from the PDF processing are sent to this class so it can process
+ * them.
+ *
+ * At the moment this is headers and footers.
+ */
+
+interface libPDFListener {
+	public function onHeader();
+	public function onFooter();
+}
